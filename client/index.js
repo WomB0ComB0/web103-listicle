8ea66df6e86f31058a0e834e38b99c860c2fc882
Mike Odnis <airwolf635@gmail.com>
2023-09-22 21:20:07 -0400

    Quick commit

--- client/index.js
+++ client/index.js
@@ -1,8 +1,27 @@
-import 'normalize.css/normalize.css'
-import './public/scss/styles.scss'
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>Test</p>
-    <button class="custom-button">Test</button>
-  </div>
-`;
+// index.js
+import 'normalize.css/normalize.css';
+import './public/scss/styles.scss';
+
+// Wrap your code in a function to ensure it's executed after the DOM is ready
+function Body() {
+  window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+    const app = document.querySelector('#app');
+      app.innerHTML = `
+        <div>
+          <p>Test</p>
+          <button class="custom-button">Test</button>
+        </div>
+      `;
+      const nav = document.querySelector('nav');
+      nav.innerHTML = `
+        <div>
+          <p>Nav</p>
+        </div>
+      `;
+      app.appendChild(nav);
+  });
+}
+
+Body();
