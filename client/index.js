b6897beb703a9867805110b2d479301e0f9d6f63
Mike Odnis <airwolf635@gmail.com>
2023-09-24 17:25:52 -0400

    Quick commit

--- client/index.js
+++ client/index.js
@@ -27,54 +27,6 @@ function Body() {
         <h1>Top Movies</h1>
       </header>
       <main>
-        <section>
-          <form>
-            <div class="grid">
-              <div class="col-12">
-                <div class="input-group">
-                  <input type="text" id="search" placeholder="Search" />
-                  <button class="btn btn-primary" id="search-btn">Search</button>
-                </div>
-              </div>
-            </div>
-            <div class="grid">
-              <div class="col-12">
-                <div class="input-group">
-                  <select id="sort">
-                    <option value="rank">Rank</option>
-                    <option value="title">Title</option>
-                    <option value="rating">Rating</option>
-                    <option value="year">Year</option>
-                  </select>
-                  <select id="order">
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
-                  </select>
-                  <button class="btn btn-primary" id="sort-btn">Sort</button>
-                </div>
-              </div>
-            </div>
-            <div class="grid">
-              <div class="col-12">
-                <div class="input-group">
-                  <select id="filter">
-                    <option value="all">All</option>
-                    <option value="title">Title</option>
-                    <option value="year">Year</option>
-                    <option value="rating">Rating</option>
-                  </select>
-                  <input type="text" id="filter-input" placeholder="Filter" />
-                  <button class="btn btn-primary" id="filter-btn">Filter</button>
-                </div>
-              </div>
-            </div>
-            <div class="grid">
-              <div class="col-12">
-                <div class="input-group">
-              </div>
-            </div>
-          </form>
-        </section>
         <section id="main-content" class="container">
           
         </section>
@@ -96,5 +48,4 @@ function Body() {
       `;
   });
 }
-// title    img    title_type    netflix_id    synopsis    rating    year    runtime    imdb_id    poster    top250    title_date
 Body();
--- client/public/scripts/titles.js
+++ client/public/scripts/titles.js
@@ -10,17 +10,47 @@ const renderMovies = async () => {
       const card = document.createElement('article');
       card.classList.add('card');
 
-      const 
+      const topContainer = document.createElement('div');
+      topContainer.classList.add('top-container');
+
+      const bottomContainer = document.createElement('div');
+      bottomContainer.classList.add('bottom-container');
+
+      topContainer.style.backgroundImage = `url(${movie.img})`;
+      
+      const title = document.createElement('h3');
+      title.textContent = movie.title;
+      bottomContainer.appendChild(title);
+
+      const rating = document.createElement('p');
+      rating.textContent = movie.rating;
+      bottomContainer.appendChild(rating);
+
+      const year = document.createElement('p');
+      year.textContent = movie.year;
+      bottomContainer.appendChild(year);
+
+      const link = document.createElement('a');
+      link.textContent = 'Read More >';
+      link.setAttribute('href', `/titles/${movie.imdb_id}`);
+      bottomContainer.appendChild(link);
+
+      card.appendChild(topContainer);
+      card.appendChild(bottomContainer);
+
+      mainContent.appendChild(card);
     })
   } else {
-
+    const message = document.createElement('h2')
+    message.textContent = 'No titles Available 😞'
+    mainContent.appendChild(message)
   }
 }
 
 const requestedUrl = window.location.href.split('/').pop()
 
 if (requestedUrl) {
-  window.location.href = '../404.html'
+  window.location.href = '../../src/page/error/404.html'
 }
 else {
   renderMovies()
