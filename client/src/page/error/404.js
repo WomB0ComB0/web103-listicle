09d95a3ac0ba005b400b085d1d645e9f26185909
Mike Odnis <airwolf635@gmail.com>
2023-09-24 17:25:52 -0400

    Quick commit

--- client/src/page/error/404.js
+++ client/src/page/error/404.js
@@ -1,9 +1,14 @@
-document.querySelector('#404').innerHTML = `
-  <section>
-    <h1>404</h1>
-    <p>Page not found.</p>
-    <button onclick="window.location.href('/')">
-      Go Home
-    </button>
-  </section>
-`;
+function FoF() {
+  window.addEventListener('load', () => {
+    document.querySelector('#404').innerHTML = `
+      <section class="f0f">
+        <h1>404</h1>
+        <p>Page not found.</p>
+        <button onclick="window.location.href('/')">
+          Go Home
+        </button>
+      </section>
+    `;
+  });
+}
+FoF();
