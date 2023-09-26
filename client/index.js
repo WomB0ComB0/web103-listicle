008732a08af3878b1a2b67194b8411a7ae4c3c46
Mike Odnis <airwolf635@gmail.com>
2023-09-26 17:29:00 -0400

    Quick commit

--- client/index.js
+++ client/index.js
@@ -1,22 +1,36 @@
 import './public/styles/sass/styles.scss';
 import { Nav, Header, Footer, Card } from './public/components/index';
-// import renderMovies from './public/scripts/titles';
 
 function Body() {
-  window.addEventListener('load', () => {
+  const content = `
+    ${Nav}
+    ${Header}
+    <main>
+      <section id="main-content" class="container">
+        ${Card}
+      </section>
+    </main>
+    ${Footer}
+  `;
+
+  if (document.readyState === 'complete') {
+
+    replaceLoadingContentWithContent();
+  } else {
+
+    window.addEventListener('load', () => {
+      replaceLoadingContentWithContent();
+    });
+  }
+
+  function replaceLoadingContentWithContent() {
     const preload = document.querySelector('.preload');
-    preload.classList.add('preload-finish');
-    const app = document.querySelector('#app');
-    app.innerHTML = `
-      ${Nav}
-      ${Header}
-      <main>
-        <section id="main-content" class="container">
-          ${Card}
-        </section>
-      </main>
-      ${Footer}
-      `;
-  });
+    if (preload) {
+      preload.classList.add('preload-finish');
+      const app = document.querySelector('#app');
+      app.innerHTML = content;
+    }
+  }
 }
+
 Body();
