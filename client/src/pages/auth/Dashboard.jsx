3a6047b1da9cfc55836f5facd90a95d67d144e46
Mike Odnis <airwolf635@gmail.com>
2023-10-09 14:21:53 -0400

    Quick commit

--- client/src/pages/auth/Dashboard.jsx
+++ client/src/pages/auth/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import CreatePreference from './crud/CreatePreference'
+import DeletePreference from './crud/DeletePreference'
+import EditPreference from './crud/EditPreference'
+import Preference from './crud/Preference'
+
 const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route element={<Preference />} />
+      <Route element={<CreatePreference />} />
+      <Route element={<EditPreference />} />
+      <Route element={<DeletePreference />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/auth/crud/CreatePreference.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreatePreference.scss'
+
+const CreatePreference = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreatePreference
